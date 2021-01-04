9e3415b3065f36eaab0a8da5a2140d7d566a745b
blockchain-jsdev <>
2021-01-04 16:02:35 +0530

    feat: new UI

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import $ from 'jquery';
+
+window.jQuery = $;
+window.$ = $;
+global.jQuery = $;
+
+const DashboardDefault = React.lazy(() => import('./Components/Dashboard/Default'));
+
+const FormsElements = React.lazy(() => import('./Components/Forms/FormsElements'));
+
+const routes = [
+  {path: '/dashboard/default', exact: true, name: 'Default', component: DashboardDefault},
+  {path: '/forms/form-basic', exact: true, name: 'Forms Elements', component: FormsElements}
+];
+
+export default routes;
