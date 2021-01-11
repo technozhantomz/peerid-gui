f3fa53c2ff6a7891d83aea2a40688e908d8a4691
blockchain-jsdev <>
2021-01-11 21:20:26 +0530

    feat: sign-in

--- src/Components/Authentication/SignIn/SignIn1.js
+++ src/Components/Authentication/SignIn/SignIn1.js
@@ -1,53 +1,87 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import {connect} from 'react-redux';
 
 // import '../../../assets/scss/style.scss';
 import Aux from "../../../hoc/_Aux";
 import Breadcrumb from "../../../App/layout/AdminLayout/Breadcrumb";
-import axios from 'axios';
+import {AuthService} from '../../../services/';
+import {GenUtil, ValidationUtil} from '../../../utility';
 
-const api = axios.create({
-    baseURL: `http://localhost:3000/`
-})
- 
+import {bindActionCreators} from 'redux';
+import {AppActions} from '../../../actions';
+import {InvalidIcon} from '../../../assets/images';
+
+const translate = GenUtil.translate;
 
 class SignUp1 extends React.Component {
 
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            login: "",
-            password: ""
+    state = {
+        username: '',
+        password: '',
+        isPasswordClicked: false,
+        isUsernameClicked: false,
+        errors:{
+          username: null,
+          password: null
         }
-    }
+      };
+    
+      handleSubmit = (event) => {
+        event.preventDefault();
+    
+        // if (this.state.errors.username !== null || this.state.errors.password !== null) {
+        //   this.setState({
+        //     loginDisabled: true
+        //   });
+    
+        //   return;
+        // }
+    
+        const account = {
+          login: this.state.username,
+          password: this.state.password
+        };
+    
+        this.props.login(account);
+      };
+    
+      handleUsernameChange = (user) => {
+        this.setState({
+          username: user,
+          isUsernameClicked: true
+        });
+      };
+    
+      handlePasswordChange = (password) => {
+        this.setState({
+          password: password,
+          isPasswordClicked: true
+        });
+      }
+    
+      logout = () => {
+        AuthService.logout();
+      };
+    
+      allowLogin = () => {
+        return (this.state.errors.username === null && this.state.errors.password === null) && (this.state.username.length && this.state.password.length);
+      };
 
-    changeHandler = e => {
-        this.setState({ [e.target.name]: e.target.value })
-    }
-
-    submitHandler = e => {
-        e.preventDefault()
-
-        api.post('/api/v1/auth/sign-in', this.state)
-            .then(res => {
-                console.log(res)
-                this.props.history.push('/dashboard/default')
-            })
-            .catch(error => {
-                console.log(error)
-            })
-
-    }
     render() {
 
-        const { login, password } = this.state
-
-        
+        const isValidUserName = () => {
+            if (this.state.isUsernameClicked) {
+              return ValidationUtil.emptyString(this.state.username).success;
+            } else {
+              return true;
+            }
+          };
+          
         return (
             <Aux>
                 <Breadcrumb />
-                <form onSubmit={this.submitHandler}>
+                <form onSubmit={this.handleSubmit}>
                     <div className="auth-wrapper">
                         <div className="auth-content">
                             <div className="auth-bg">
@@ -64,21 +98,26 @@ class SignUp1 extends React.Component {
                                     <h3 className="mb-4">Login</h3>
 
                                     <div className="input-group mb-3">
-                                        <input name="login" className="form-control" placeholder="Email"
-                                            value={login}
-                                            onChange={this.changeHandler}
-                                            required
+                                        <input 
+                                            name='username'
+                                            hasActiveGlow={ true }
+                                            placeholder={ translate('login.enterUsername') }
+                                            handleChange={ this.handleUsernameChange }
+                                            iconRightActive={ InvalidIcon }
+                                            isValid={ isValidUserName }
+                                            // handleRightIconClick={ rightIconClickHandler }
                                         />
                                     </div>
                                     <div className="input-group mb-4">
-                                        <input name='password' className="form-control" placeholder="password"
-                                            value={password}
-                                            onChange={this.changeHandler}
-                                            type="password"
-                                            required
+                                        <input 
+                                            name='password'
+                                            type='password'
+                                            hasActiveGlow={ true }
+                                            placeholder={ translate('login.enterPassword') }
+                                            handleChange={ this.handlePasswordChange }
                                         />
                                     </div>
-                                    <button type="submit" className="btn btn-primary shadow-2 mb-4">Login</button>
+                                    <button type="submit"  className="btn btn-primary shadow-2 mb-4">Login</button>
                                     <p className="mb-2 text-muted">Forgot password? <NavLink to="/auth/reset-password-1">Reset</NavLink></p>
                                     <p className="mb-0 text-muted">Don’t have an account? <NavLink to="/auth/signup-1">Signup</NavLink></p>
                                     {/* <LoginFooter></LoginFooter> */}
@@ -92,4 +131,16 @@ class SignUp1 extends React.Component {
     }
 }
 
-export default SignUp1;
+const mapStateToProps = (state) => ({isLoggedIn: state.getIn(['profiles', 'isLoggedIn'])});
+
+const mapDispatchToProps = (dispatch) => bindActionCreators(
+  {
+    login: AppActions.login,
+  },
+  dispatch
+);
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(SignUp1);
--- src/route.js
+++ src/route.js
@@ -2,10 +2,14 @@ import React from 'react';
 
 const SignUp1 = React.lazy(() => import('./Components/Authentication/SignUp/SignUp1'));
 const Signin1 = React.lazy(() => import('./Components/Authentication/SignIn/SignIn1'));
+const DashboardDefault = React.lazy(() => import('./Components/Dashboard/Default'));
+const FormsElements = React.lazy(() => import('./Components/Forms/FormsElements'));
 
 const route = [
     { path: '/auth/signup-1', exact: true, name: 'Signup 1', component: SignUp1 },
-    { path: '/auth/signin-1', exact: true, name: 'Signin 1', component: Signin1 }
+    { path: '/auth/signin-1', exact: true, name: 'Signin 1', component: Signin1 },
+    { path: '/dashboard/default', exact: true, name: 'Default', component: DashboardDefault },
+    { path: '/forms/form-basic', exact: true, name: 'Forms Elements', component: FormsElements },
 ];
 
 export default route;
